0c3f4ce011ebaca974ac345e1383b65e02a70322
abhirami-vinod <144613573+abhirami-vinod@users.noreply.github.com>
2024-03-26 15:00:53 +0530

    Add files via upload

---
+++ c2/StringSort.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class StringSort
+{
+    public static void main(String[] args)
+    {
+        int n;
+        String temp;
+        Scanner s = new Scanner(System.in);
+        System.out.print(" Abhirami Vinod\n 23MCA002\n 26-FEB-2024\n");
+        System.out.print("Enter number of strings you want to enter:\n");
+        n = s.nextInt();
+        String strings[] = new String[n];
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Enter all the Strings:");
+        for(int i = 0; i < n; i++)
+        {
+            strings[i] = s1.nextLine();
+        }
+        System.out.println("Array: " + Arrays.toString(strings));
+Arrays.sort(strings);
+System.out.println("Sorted Array: " + Arrays.toString(strings));
+    }
+}
