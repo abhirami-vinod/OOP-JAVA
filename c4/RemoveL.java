5888b15045ff655186df36d77deb7f90871949b1
abhirami-vinod <144613573+abhirami-vinod@users.noreply.github.com>
2024-04-15 12:01:31 +0530

    Add files via upload

---
+++ c4/RemoveL.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class RemoveL {
+public static void main(String[] args){
+System.out.println("Abhirami Vinod\n23MCA002\n16-04-2024");
+LinkedList<String> L=new LinkedList<>();
+L.add("Gold");
+L.add("Silver");
+L.add("Bronze");
+L.add(0,"Olympics Medals");
+System.out.println(L);
+L.remove("Bronze");
+System.out.println(L);
+L.remove(2);
+System.out.println(L);
+L.removeLast();
+System.out.println(L);
+L.removeFirst();
+System.out.println(L);
+}
+}
